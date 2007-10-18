00766714dcd63074d2b1db9ac767a50fc5c4e167
Marcel Grünauer <marcel@cpan.org>
2007-10-18 17:45:51 +0000

    Import of MARCEL/Net-IP-Match-0.02 from CPAN.
    gitpan-cpan-distribution: Net-IP-Match
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         MARCEL/Net-IP-Match-0.02.tar.gz
    gitpan-cpan-author:       MARCEL
    gitpan-cpan-maturity:     released

--- lib/Net/IP/Match.pm
+++ lib/Net/IP/Match.pm
@@ -5,7 +5,7 @@ use strict;
 use warnings;
 use Filter::Simple;
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 FILTER sub {
     	s[\b __MATCH_IP \s* \( (.*?) \s* , \s* (.*?) \s* \) ]
@@ -148,24 +148,39 @@ would have to call it as a function, which would add the stack
 overhead. Richard Clamp had the interesting idea of optimizing the
 generated opcode tree.
 
-=head1 BUGS
+=head1 TAGS
 
-None known so far. If you find any bugs or oddities, please do
-inform the author.
+If you talk about this module in blogs, on del.icio.us or anywhere else,
+please use the C<netipmatch> tag.
+
+=head1 BUGS AND LIMITATIONS
+
+No bugs have been reported.
+
+Please report any bugs or feature requests to
+C<bug-net-ip-match@rt.cpan.org>, or through the web interface at
+L<http://rt.cpan.org>.
+
+=head1 INSTALLATION
+
+See perlmodinstall for information and options on installing Perl modules.
+
+=head1 AVAILABILITY
+
+The latest version of this module is available from the Comprehensive Perl
+Archive Network (CPAN). Visit <http://www.perl.com/CPAN/> to find a CPAN
+site near you. Or see <http://www.perl.com/CPAN/authors/id/M/MA/MARCEL/>.
 
 =head1 AUTHOR
 
-Marcel GrE<uuml>nauer, <marcel@cpan.org>
+Marcel GrE<uuml>nauer, C<< <marcel@cpan.org> >>
 
-=head1 COPYRIGHT
+=head1 COPYRIGHT AND LICENSE
 
-Copyright 2002 Marcel GrE<uuml>nauer. All rights reserved.
+Copyright 2002-2007 by Marcel GrE<uuml>nauer
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
 
-=head1 SEE ALSO
-
-perl(1).
-
 =cut
+
